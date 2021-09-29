5a8ee4989c582741736e7ef1844db14af86ffc1d
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-09-29 12:01:49 +0800

    Refactor for new rstantools

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,2 @@
-.onLoad <- function(libname, pkgname) {
-  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-  for (m in modules) loadModule(m, what = TRUE)
-}
-
 if(getRversion() >= "3.1.0") utils::suppressForeignCheck("dose")
 if(getRversion() >= "2.15.1")  utils::globalVariables("dose")
